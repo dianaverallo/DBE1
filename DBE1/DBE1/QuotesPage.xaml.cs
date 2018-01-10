6fcde254a7c6383ab000507d3616b7876866bbec
Diana Verallo <001649@lrmc.com.ph>
2018-01-10 15:37:38 +0800

    Add project files.

---
+++ DBE1/DBE1/QuotesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DBE1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotesPage : ContentPage
+	{
+	    private int _index = 0;
+	    private string[] _arrquotes = new string[]
+	    {
+            "You can't blame gravity for falling in love.",
+	        "Next quote.",
+	        "Last quote."
+	    };
+
+	    public QuotesPage()
+	    {
+	        InitializeComponent();
+	        myQuote.Text = _arrquotes[_index];
+	    }
+
+	    void Handle_Clicked(object sender, System.EventArgs e)
+	    {
+	        _index++;
+	        if (_index >= _arrquotes.Length)
+	            _index = 0;
+
+	        myQuote.Text = _arrquotes[_index];
+	    }
+    }
+}
